20f6891a359cba05c66a3a92bc3eac0e5ed65ee5
Hax0l0tl <sspielmann@babbel.com>
2022-12-15 14:37:39 +0100

    run a test using the fizzchecker function

--- main.ts
+++ main.ts
@@ -3,11 +3,17 @@ function fizz(n: number) {
     return (n % 3 == 0) ? 'fizz' : n;
 } 
 
-function fizzChekcer(n: number, result: string) {
-    return fizz(n) == result ? true : false;
+function fizzChecker(n: number, result: string) {
+    return fizz(n) == result;
     
 }
 
+var testsPassed = true; 
+const expectedOutput = ['fizz', '1', '2', 'fizz', '4', '5', 'fizz', '7', '8', 'fizz', '10', '11', 'fizz', '13', '14', 'fizz', '16', '17', 'fizz', '19', '20'];
+
 for (let i = 1; i < 20; i++) {
-   console.log(fizzChekcer(i, 'fizz'));
+   if (!fizzChecker(i, expectedOutput[i])) {
+    testsPassed = false;
+   };
 }
+console.log(testsPassed ? 'GREEN' : 'RED');
